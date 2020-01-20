3e865aa1fa24581fc1c52a7b8036a872a88cb64b
Gavrilenko <gavrilenko.georgi@gmail.com>
2020-01-20 13:55:35 +0200

    Add footer icons and fix main page text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import { footerDate } from '../services/dateAndTime'
+import { Container, Row, Col } from 'react-bootstrap'
+
+import { ReactComponent as FbIcon } from '../svg/facebook.svg'
+import { ReactComponent as InstIcon } from '../svg/instagram.svg'
 
 const Footer = () => {
+	const maxWidth = {
+		maxWidth: '1.7em',
+		margin: '0em .5em'
+	}
 	return (
 		<footer className="footer">
-			<div className="container">
-				<span className="text-muted">&copy;&nbsp;{footerDate()}&nbsp;Artviva</span>
-			</div>
+			<Container>
+				<Row>
+					<Col xs={8}>
+						<span className="text-muted">
+							&copy;&nbsp;{footerDate()}&nbsp;Artviva
+						</span>
+					</Col>
+					<Col xs={4} className="text-center">
+						<a
+							href="https://www.facebook.com/myz.shpytky"
+							alt="Фейсбук-група Шпитьківської Дитячої Школи Мистецтв"
+						>
+							<FbIcon style={maxWidth} />
+						</a>
+						<a
+							href="https://www.instagram.com/myz_shputky"
+							alt="Інстаграм Шпитьківської Дитячої Школи Мистецтв"
+						>
+							<InstIcon style={maxWidth} />
+						</a>
+					</Col>
+				</Row>
+			</Container>
 		</footer>
 	)
 }
