085fb5f13af26db17c3ba3dc045bde4d5dcbdb1d
Gavrilenko <gavrilenko.georgi@gmail.com>
2020-02-07 13:27:52 +0200

    Fix navbar active class naming param

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -22,7 +22,7 @@ const NavigationBar = ({ user }) => {
 					<Nav className="ml-auto pl-2 d-flex align-items-left">
 
 						<Link to="/about" className="d-flex align-items-center">
-							<Nav.Link as="span" href="/about" activeClassName="active">
+							<Nav.Link as="span" href="/about" activeclassname="active">
 								Про школу
 							</Nav.Link>
 						</Link>
@@ -35,19 +35,19 @@ const NavigationBar = ({ user }) => {
 						</Link>*/}
 
 						<Link to="/teachers" className="d-flex align-items-center">
-							<Nav.Link as="span" href="/teachers" activeClassName="active">
+							<Nav.Link as="span" href="/teachers" activeclassname="active">
 								Наші вчителі
 							</Nav.Link>
 						</Link>
 
 						<Link to="/blog" className="d-flex align-items-center">
-							<Nav.Link as="span" href="/blog" activeClassName="active">
+							<Nav.Link as="span" href="/blog" activeclassname="active">
 								Блог
 							</Nav.Link>
 						</Link>
 
 						<Link to="/contacts" className="d-flex align-items-center">
-							<Nav.Link as="span" href="/contacts" activeClassName="active">
+							<Nav.Link as="span" href="/contacts" activeclassname="active">
 								Контакти
 							</Nav.Link>
 						</Link>
@@ -56,7 +56,7 @@ const NavigationBar = ({ user }) => {
 							className="d-flex justify-content-end"
 							href="/login"
 							as="span"
-							activeClassName="active"
+							activeclassname="active"
 						>
 							{user
 								? <>
